842a251d81ed93df04480e5538c15bd76a6dca72
Scott Yim <scottyim@Scotts-MacBook-Pro.local>
2020-04-11 13:26:41 -0400

    updated config.py with VCAP SERVICES check

--- config.py
+++ config.py
@@ -2,13 +2,15 @@
 Global Configuration for Application
 """
 import os
-
+import json
 # Get configuration from environment
 DATABASE_URI = os.getenv("DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres")
-
+# override if we are running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
